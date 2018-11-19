3fd65cb34a72c8c8a8dc4c9b6c88ff90cf506ffb
John Doe <johndoe@example.com>
2018-11-19 15:35:36 +0800

    v2

--- routes/foodtopia.js
+++ routes/foodtopia.js
@@ -20,10 +20,37 @@ connection.connect(function(err) {
 router
   .route("/menu")
   .get(function(req, res) {
-    connection.query("Select * from menu", function(error, results) {
+    connection.query("Select * from menu", function(error, rows) {
       if (error) throw error;
-      res.json(results);
+      res.json(rows);
     });
   });
+
+  router
+  .route("/menu/:page")
+  .get(function (req, res) {
+    //先統計總共幾筆資料
+    var query = "select count(*) as TotalCount from menu"; //用SQL找總共多少筆
+    var totalCount = 0;
+    connection.query(query, function (error, row) {
+      if (error) throw error;
+      totalCount = row[0].TotalCount;
+
+      //讀出分頁資料
+      var LimitNum = 6;   //一次讀取6筆資料
+      var startNum = 0;    //從第幾筆開始讀
+      if (req.params.page) {                //?
+        page = parseInt(req.params.page); //parseInt化
+        startNum = (page - 1) * LimitNum; //依據頁數讀取第一筆的項目id
+      }
+      var query = "select * from menu ORDER BY `id` DESC limit ? OFFSET ?"; //每頁項目範圍
+      var params = [LimitNum, startNum];
+      query = mysql.format(query, params); //format 將query取得的項目轉化成params格式
+      connection.query(query, function (error, row) {
+        if (error) throw error;
+        res.json({ "TotalCount": totalCount, "datas": row });
+      });
+    });
+  })
 
 module.exports = router;
