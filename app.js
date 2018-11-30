b48c75dfa6a6c791335a93c61a0bc9fe241d338b
yvonne <yvonne250003@gmail.com>
2018-11-30 11:14:53 +0800

    recipe api

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var recipeRouter = require('./routes/recipe');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +22,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// http://localhost:3000
 app.use('/', indexRouter);
+// http://localhost:3000/users
 app.use('/users', usersRouter);
+// http://localhost:3000/api
+app.use('/api', recipeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
